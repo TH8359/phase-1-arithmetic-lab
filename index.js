a17a32501d0729c06c2ea7668c7dd46e7f64573c
Tyler Hogue <hogue73@embarqmail.com>
2024-12-03 20:40:09 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+let num3 = 44
+let num4 = 10
+let multiply
+let random
+let mod
+let max
 
-// Write your code here
+multiply = num1 * num2
+
+random = Math.ceil(Math.random() * 5) + 1
+
+mod = num3 % num4
+
+max = Math.max(5, 10, 15, 20)
